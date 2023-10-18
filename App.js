4086aabaa6e68d8c79526d19ae716d531d468827
Yolène <91735106+Yawleen@users.noreply.github.com>
2023-10-18 12:21:39 +0200

    first API call to display "Hello World!"

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [data, setData] = useState("");
+
+  const fetchMessage = () => {
+    fetch(process.env.EXPO_PUBLIC_API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.message);
+      })
+      .catch((error) => {
+        console.error("Erreur de requête :", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{data}</Text>
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
