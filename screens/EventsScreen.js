52fcbd4c90d23b8240e48bf6aaab9a226d307273
Yolène <yolene@brokeandabroad.com>
2024-02-07 14:05:18 +0100

    add a genres filter for events

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -6,6 +6,8 @@ import { getValueFor } from "../helpers/secureStore";
 import { Searchbar } from "react-native-paper";
 import { useScrollToTop } from "@react-navigation/native";
 import { useCollapsibleHeader } from "react-navigation-collapsible";
+import Modal from "react-native-modal";
+import { CheckBox } from "@rneui/themed";
 import {
   StyleSheet,
   View,
@@ -15,9 +17,11 @@ import {
   Text,
   Animated,
   Platform,
+  FlatList,
 } from "react-native";
 import EventCard from "../components/EventCard";
 import IconButton from "../components/IconButton";
+import Button from "../components/Button";
 
 export default function EventsScreen() {
   const flatList = useRef();
@@ -30,6 +34,10 @@ export default function EventsScreen() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSearchLoading, setIsSearchLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [showGenres, setShowGenres] = useState(false);
+  const [isGenresLoading, setIsGenresLoading] = useState(false);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
   const { onScroll, scrollIndicatorInsetTop, translateY } =
     useCollapsibleHeader({
       navigationOptions: {
@@ -53,7 +61,11 @@ export default function EventsScreen() {
       const token = await getValueFor(AUTH_TOKEN);
       if (token) {
         await fetch(
-          `${process.env.EXPO_PUBLIC_API_URL}/events?page=${page}&eventName=${searchQuery}`,
+          `${
+            process.env.EXPO_PUBLIC_API_URL
+          }/events?page=${page}&eventName=${searchQuery}&genres=${selectedGenres.join(
+            ","
+          )}`,
           requestOptions("GET", token)
         ).then((response) => {
           response.json().then((data) => {
@@ -103,16 +115,70 @@ export default function EventsScreen() {
     await fetchEvents(searchQuery);
     setIsSearchLoading(false);
     onPressFunction();
+    if (showGenres) {
+      setShowGenres(false);
+    }
   };
 
   const renderEventCard = ({ item }) => <EventCard eventInfo={item} />;
 
   const setKeyExtractor = (item) => item._id;
 
+  const toggleGenresModal = () => setShowGenres(!showGenres);
+
+  const fetchGenres = async () => {
+    setIsGenresLoading(true);
+
+    try {
+      const token = await getValueFor(AUTH_TOKEN);
+      if (token) {
+        await fetch(
+          `${process.env.EXPO_PUBLIC_API_URL}/genres`,
+          requestOptions("GET", token)
+        ).then((response) => {
+          response.json().then((data) => {
+            setGenres(data.genres);
+            setIsGenresLoading(false);
+          });
+        });
+      }
+    } catch (error) {
+      Alert.alert("Erreur", error.message);
+      setIsGenresLoading(false);
+    }
+  };
+
+  const renderGender = ({ item }) => (
+    <CheckBox
+      checked={selectedGenres.includes(item.name)}
+      checkedColor={item.color}
+      uncheckedColor="#111"
+      textStyle={styles.checkBoxText}
+      title={item.name}
+      onPress={() => handleGenresSelection(item.name)}
+    />
+  );
+
+  const handleGenresSelection = (genre) => {
+    if (selectedGenres.includes(genre)) {
+      const selectedGenresCopy = [...selectedGenres];
+      const genreIndex = selectedGenresCopy.findIndex(
+        (selectedGenre) => selectedGenre === genre
+      );
+      selectedGenresCopy.splice(genreIndex, 1);
+      setSelectedGenres(selectedGenresCopy);
+      return;
+    }
+
+    setSelectedGenres([...selectedGenres, genre]);
+  };
+
   useEffect(() => {
     if (!eventsInfo.isLastPage) {
       fetchEvents();
     }
+
+    fetchGenres();
   }, []);
 
   return (
@@ -152,7 +218,6 @@ export default function EventsScreen() {
           })}
           ItemSeparatorComponent={<View style={styles.separator}></View>}
         />
-
         <Animated.View
           style={[
             {
@@ -174,6 +239,14 @@ export default function EventsScreen() {
               !isLoading && { onSubmitEditing: onSubmitSearch })}
             onClearIconPress={clearSearchInput}
           />
+          <View style={styles.genresFilter}>
+            <IconButton
+              icon="music"
+              size={20}
+              color="#111"
+              onPress={toggleGenresModal}
+            />
+          </View>
         </Animated.View>
       </View>
       {eventsInfo.events.length > 0 && (
@@ -187,6 +260,46 @@ export default function EventsScreen() {
             </Text>
           </View>
         </>
+      )}
+      {showGenres && (
+        <Modal
+          isVisible={showGenres}
+          backdropColor="#111"
+          backdropOpacity={0.6}
+          onBackdropPress={toggleGenresModal}
+          hideModalContentWhileAnimating={true}
+        >
+          <View style={styles.modal}>
+            <View style={styles.closeIconContainer}>
+              <Text style={styles.genresText}>Filtrer par genre(s) :</Text>
+              <IconButton
+                icon="close"
+                size={26}
+                color="#111"
+                onPress={toggleGenresModal}
+              />
+            </View>
+            <FlatList
+              data={genres}
+              renderItem={renderGender}
+              keyExtractor={setKeyExtractor}
+              {...(isGenresLoading && {
+                ListFooterComponent: () => (
+                  <ActivityIndicator
+                    size="large"
+                    style={styles.loader}
+                    color={Colors.primary600}
+                  />
+                ),
+              })}
+            />
+            {!isGenresLoading && (
+              <View style={styles.buttonContainer}>
+                <Button text="Filtrer" onPress={onSubmitSearch} />
+              </View>
+            )}
+          </View>
+        </Modal>
       )}
     </View>
   );
@@ -250,6 +363,8 @@ const styles = StyleSheet.create({
   },
   searchBarContainer: {
     position: "absolute",
+    flexDirection: "row",
+    columnGap: 5,
     width: "100%",
     paddingHorizontal: 10,
     paddingVertical: 16,
@@ -258,10 +373,37 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 6,
   },
   searchBar: {
+    flexGrow: 1,
     borderRadius: 10,
     backgroundColor: "#fff",
   },
   searchBarInput: {
     fontFamily: "openSansRegular",
+  },
+  genresFilter: {
+    flexShrink: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  modal: {
+    backgroundColor: "#fff",
+    padding: 30,
+    borderRadius: 20,
+  },
+  closeIconContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  genresText: {
+    fontSize: 15,
+    fontFamily: "openSansBold",
+    marginBottom: 10,
+  },
+  checkBoxText: { fontFamily: "openSansRegular", fontSize: 15 },
+  buttonContainer: {
+    marginTop: 15,
   },
 });
